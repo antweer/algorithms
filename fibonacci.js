8c61b8b609257c63ce012f44a7afc7c726a57ee8
antweer <trajwani@gmail.com>
2017-06-01 19:33:25 -0500

    non recursive fibonacci, recursive exponential function, and non recursive multiplication function algorithm added)

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,15 @@
 //Fibonnaci algorithm
-
+function fibonnaci(num){
+  temp = 0;
+  post = 0;
+  val = 1;
+  for(i=0; i<=num; i++){
+    fib = post;
+    post = val;
+    val += fib;
+  }
+  return fib;
+}
 //Recursive fibonnaci algorithm
 function fibonnaci(num){
   let val = 0;
@@ -12,3 +22,17 @@ function fibonnaci(num){
   return fibonnaci(num-1) + fibonnaci(num-2);
 }
 
+//Bonus recursive exponential function
+function exp(num, pwr){
+  if(pwr === 0) return 1;
+  return num*exp(num, pwr-1);
+}
+
+//Bonus X*Y 
+function mult(x,y){
+  let val = 0;
+  for(let i=1; i<=y; i++){
+    val += x;
+  }
+  return val;
+}
